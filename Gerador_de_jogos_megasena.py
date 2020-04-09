277abab8783da5ead70b0e3bcec90dc319224629
Paulo B. Carvalho <paulo.bonfleur@gmail.com>
2020-04-09 15:51:24 -0300

    Primeira versão

---
+++ Gerador_de_jogos_megasena.py
@@ -0,0 +1,55 @@
+import PySimpleGUI as sg
+from random import randint
+
+
+class telapython:
+    def __init__(self):
+        # Layout
+        layout = [
+            [sg.Text('Digite a quantidade de jogos que deseja fazer :'),sg.Input(key='Digite a quantidade de jogos que deseja fazer :')],
+            [sg.Button('Criar Jogo')],
+            [sg.Output(size=(30, 20))]
+        ]
+        #Janela
+        self.janela = sg.Window('Gerador de Jogos da Megasena').layout(layout)
+        #Dados
+        self.button, self.values = self.janela.Read()
+
+    def mega_sena(self, n):
+        p = set()
+        im = set()
+        result = []
+        for i in range(int(n)):
+            while len(p) < 3:
+                r = randint(1, 60)
+                if r % 2 == 0 and r not in p:
+                    p.add(r)
+            while len(im) < 3:
+                r = randint(1, 60)
+                if r % 2 != 0 and r not in p:
+                    im.add(r)
+            for x in p:
+                result.append(x)
+            for x in im:
+                result.append(x)
+            print(result)
+            p = set()
+            im = set()
+            result = []
+
+
+
+    def iniciar(self):
+        if telapython.mega_sena(self, self.values['Digite a quantidade de jogos que deseja fazer :']) != None:
+            print(telapython.mega_sena(self, self.values['Digite a quantidade de jogos que deseja fazer :']))
+
+        while True:
+            self.button, self.values = self.janela.Read()
+            print()
+            if telapython.mega_sena(self, self.values['Digite a quantidade de jogos que deseja fazer :']) != None:
+                print(telapython.mega_sena(self, self.values['Digite a quantidade de jogos que deseja fazer :']))
+
+
+tela = telapython()
+tela.iniciar()
+
